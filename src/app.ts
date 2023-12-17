776fbebe1e8a089294139adaaac2e6b0e366ea93
Hyunin Sim <holasim91@gmail.com>
2023-12-17 22:23:47 +0900

    불필요한 코드 삭제, 고정 url이 아닌 path로 변경

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import apiRoute from './router/index';
 import axios from 'axios';
 const schedule = require('node-schedule');
@@ -10,14 +10,11 @@ import { getSubsInfoFromYoutube } from './api/external';
 
 dotenv.config()
 
-const youtube = google.youtube({ version: 'v3', auth: process.env.YOUTUBE_API_KEY });
-
-
 const app = express();
+
 require('dotenv').config();
 
 app.use(express.json());
-
 app.use('/api', apiRoute);
 
 
@@ -26,20 +23,16 @@ schedule.scheduleJob('55 23 * * *', async function () { // sec(option) min hour 
     const targetURL = 'http://localhost:3001/api/user'
     const postURL = 'http://localhost:3001/api/subs/add_sub_info'
     try {
-        const response = await axios.get(targetURL)
+        const response = await axios.get('/api/user')
         const channelIds = response.data.map((r: { id: string; }) => r.id)
         console.log(channelIds)
         if (channelIds.length > 0) {
             const aaa = await getSubsInfoFromYoutube(channelIds)
             console.log(aaa, '?')
             if (aaa) {
-                axios.post(postURL, aaa)
+                axios.post('/api/subs/add_sub_info', aaa)
             }
-
         }
-
-
-        // 
     } catch (e) {
         console.error(e)
     }
@@ -47,7 +40,6 @@ schedule.scheduleJob('55 23 * * *', async function () { // sec(option) min hour 
 });
 
 app.set("port", process.env.PORT || 5001);
-
 app.listen(app.get("port"), () => {
     console.log(`Server listening on port: ${app.get("port")}`);
 })
