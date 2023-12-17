ac4203b355a63693dc85c080995fa1379369248f
Hyunin Sim <holasim91@gmail.com>
2023-12-17 23:01:43 +0900

    스케쥴링 시간 메인 시간대로 수정

--- src/app.ts
+++ src/app.ts
@@ -18,18 +18,16 @@ app.use('/api', apiRoute);
 
 const testBatchTime = '10 * * * * *'
 const batchTime = "0 55 23 * * *"
-schedule.scheduleJob(testBatchTime, async () => { // sec(option) min hour day month 요일
+schedule.scheduleJob(batchTime, async () => { // sec(option) min hour day month 요일
     console.log('TIME TO GET SUBSCRIBER INFO OF BEDORAZI')
     const targetURL = 'http://localhost:3001/api/user'
     const postURL = 'http://localhost:3001/api/subs/add_sub_info'
     try {
         const response = await axios.get(targetURL)
-        console.log(response,'?')
         const channelIds = response.data.map((r: { id: string; }) => r.id)
         console.log(channelIds)
         if (channelIds.length > 0) {
             const aaa = await getSubsInfoFromYoutube(channelIds)
-            console.log(aaa, '?')
             if (aaa) {
                 axios.post(postURL, aaa)
             }else{
